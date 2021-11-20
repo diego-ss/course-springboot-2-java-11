571b3b34efacea564d726b812815b571cb1fb065
Diego <diego_sousa.s@outlook.com>
2021-11-20 13:54:02 -0300

    Order, Instant, ISO8601 part 1

--- src/main/java/com/diegoss/course/entities/User.java
+++ src/main/java/com/diegoss/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.diegoss.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity 
+@Table(name="tb_user")
 public class User implements Serializable{
 
 	/**
@@ -23,6 +28,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -51,6 +59,10 @@ public class User implements Serializable{
 	
 	public String getEmail() {
 		return email;
+	}
+	
+	public List<Order> getOrders(){
+		return orders;
 	}
 
 	public void setEmail(String email) {
